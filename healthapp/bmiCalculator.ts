06ac0b687ffe7716703affbe997c86c4cdb41fee
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2026-09-04 10:30:35 +0300

    5. WebBmi

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -18,21 +18,26 @@ const parseArguments = (args: Array<string>): CalculateValues => {
     }
   }
 
-const calculateBmi = (a: number, b: number) => {
-    const pituus = a * 0.01;
-    const paino = b;
+export const calculateBmi = (pituus: number, paino: number) => {
+    pituus = pituus * 0.01;
     const bmi = paino / ( pituus * pituus );
     const result = 25 <= bmi ? 'Overweight' : 19 <= bmi ? 'Normal' : 'Underweight';
     console.log(result);
+    return result + ' range';
 };
 
-try {
-    const { value1, value2 } = parseArguments(process.argv);
-    console.log(calculateBmi(value1, value2));
-} catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-        errorMessage += ' Error: ' + error.message;
-    }
-    console.log(errorMessage);
+if (process.argv[1] === import.meta.filename) {
+  // do not run this code if module is imported
+  try {
+      const { value1, value2 } = parseArguments(process.argv);
+      console.log(calculateBmi(value1, value2));
+  } catch (error: unknown) {
+      let errorMessage = 'Something went wrong.';
+      if (error instanceof Error) {
+          errorMessage += ' Error: ' + error.message;
+      }
+      console.log(errorMessage);
+  }
 }
+
+
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,9 +1,32 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator.ts';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!height || !weight) {
+    res.status(400).json({ error: 'content missing' });
+  }
+
+  const heightNum = Number(height);
+  const weightNum = Number(weight);
+
+  if (isNaN(heightNum) || isNaN(weightNum)) {
+    res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const bmiValue = calculateBmi(heightNum, weightNum);
+  res.json({
+    weight: weightNum,
+    height: heightNum,
+    bmi: bmiValue
+  });
 });
 
 const PORT = 3003;
